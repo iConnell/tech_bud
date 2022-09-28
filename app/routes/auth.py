9d58ede97c335038540f4ca00c9daf1ba71e723f
McConnell <ik.ugwuanyi@gmail.com>
2022-09-28 20:54:49 +0100

    login route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 from ..schemas.users import RegistrationSchema
 from ..models.users import User
-from .utils import hash_password, create_access_token
+from .utils import hash_password, create_access_token, verify_password
 
 
 router = APIRouter(prefix='/api/auth')
@@ -26,3 +26,15 @@ def create_user(request: RegistrationSchema, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"token":create_access_token({"email": new_user.email, "id":new_user.id})}
+
+
+@router.post('/login', status_code=status.HTTP_200_OK)
+def user_login(request: RegistrationSchema, db: Session=Depends(get_db)):
+    user = db.query(User).filter(User.email==request.email).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials provied")
+    
+    if not verify_password(request.password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials provied")
+
+    return {"token":create_access_token({"email": user.email, "id":user.id})}
