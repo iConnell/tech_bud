8f6c7ac786539d05ad92fc97e54d34d0190defe7
McConnell <ik.ugwuanyi@gmail.com>
2022-09-30 23:28:14 +0100

    post get routes

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from ..database import get_db
@@ -22,3 +22,18 @@ def create_post(request: PostCreateSchema, user: ReadUser = Depends(get_current_
     db.refresh(new_post)
 
     return new_post
+
+
+@router.get('', status_code=status.HTTP_200_OK, response_model=List[PostReadSchema], dependencies=[Depends(get_current_user)])
+def get_all_posts(db: Session = Depends(get_db)):
+    posts = db.query(Post).all()
+    return posts
+
+@router.get('/{post_id}', status_code=status.HTTP_200_OK, response_model=PostReadSchema, dependencies=[Depends(get_current_user)])
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id==post_id).first()
+
+    if not post:
+        raise HTTPException(status_code=404, detail=f"post with id {post_id} not found")
+
+    return post
